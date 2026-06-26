b4e0fa2d5d06813f9bbd1139c573b0308a804d59
msayracode <msayracode@gmail.com>
2026-06-26 10:17:11 +0300

    Serializer

--- profiller/signals.py
+++ profiller/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from profiller.models import Profil
+from profiller.models import Profil, ProfilDurum
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,6 +8,15 @@ def create_profil(sender, instance, created, **kwargs):
     print(instance.username, ' __Created : ', created)
     if created:
         Profil.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Profil)
+def create_ilk_durum_mesaji(sender, instance, created, **kwargs):
+    if created:
+        ProfilDurum.objects.create(
+            user_profil=instance,
+            durum_mesaji = f'{instance.user.username} Klube katıldı.'            
+        )
 
 
 
